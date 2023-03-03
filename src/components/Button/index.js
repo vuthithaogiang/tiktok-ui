a348efa3658620807513189e506bca39c5adad84
giang <vuthithaogiang050400@gmail.com>
2023-03-03 17:23:24 +0700

    add reset style css - normalize.css

--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import myAlert from "~/myAlert"
-
-function Button () {
-    return (
-        <button onClick={myAlert}>click me!</button>
-    )
-}
-
-export default Button 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from '~/components/GlobalStyles';
 
 ReactDOM.render(
-  <App/>, document.querySelector('#root')
+    <GlobalStyles>
+        <App />
+    </GlobalStyles>,
+    document.querySelector('#root'),
 );
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
