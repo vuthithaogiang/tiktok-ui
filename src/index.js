ba03938e90a9814a191f1a214fa01fa290a1a2b3
giang <vuthithaogiang050400@gmail.com>
2023-03-06 10:03:54 +0700

    router layout

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import GlobalStyles from '~/components/GlobalStyles';
+import GlobalStyles from './components/GlobalStyles';
 
 ReactDOM.render(
     <GlobalStyles>
